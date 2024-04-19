fd939264e16bc63b7925f4d53a3dbe9ebee37080
madinaadzhi <madinaadzhigeldieva2004@gmail.com>
2024-04-19 17:28:43 +0300

    new implementation fox algorithm

--- lab2/src/main/java/org/madi/lab2/fox/Fox.java
+++ lab2/src/main/java/org/madi/lab2/fox/Fox.java
@@ -1,9 +1,13 @@
 package org.madi.lab2.fox;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.madi.lab2.Matrix;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Getter
 @Setter
@@ -12,45 +16,100 @@ public class Fox {
     private Matrix matrix1;
     private Matrix matrix2;
     private int threadsCnt;
-    private Matrix resultMatrix;
 
     public Fox(Matrix Matrix1, Matrix Matrix2, int threadsCnt) {
         this.matrix1 = Matrix1;
         this.matrix2 = Matrix2;
-        this.resultMatrix = new Matrix(Matrix1.getRowSize(), Matrix2.getColumnSize());
-        if (threadsCnt > Matrix1.getRowSize() * Matrix2.getColumnSize() / 4) {
-            this.threadsCnt = Matrix1.getRowSize() * Matrix2.getColumnSize() / 4;
-        } else {
-            this.threadsCnt = Math.max(threadsCnt, 1);
+        this.threadsCnt = threadsCnt;
+    }
+
+    private int findDivider(int s, int p) {
+        int i = s;
+        while (i > 1) {
+            if (p % i == 0) {
+                break;
+            }
+            if (i >= s) {
+                i++;
+            } else {
+                i--;
+            }
+            if (i > Math.sqrt(p)) i = Math.min(s, p / s) - 1;
         }
+        return i >= s ? i : i != 0 ? p / i : p;
     }
 
     public Matrix multiply() {
-        int step = (int) Math.ceil(1.0 * matrix1.getRowSize() / (int) Math.sqrt(threadsCnt));
+        Matrix resultMatrix = new Matrix(matrix1.getRowSize(), matrix2.getColumnSize());
 
-        FoxThread[] threads = new FoxThread[threadsCnt];
-        int cnt = 0;
-
-        for (int i = 0; i < matrix1.getRowSize(); i += step) {
-            for (int j = 0; j < matrix2.getColumnSize(); j += step) {
-                threads[cnt] = new FoxThread(matrix1, matrix2, i, j, step, resultMatrix);
-                cnt++;
+        if (!(matrix1.getRowSize() == matrix1.getColumnSize()
+                & matrix2.getRowSize() == matrix2.getColumnSize()
+                & matrix1.getRowSize() == matrix2.getRowSize())) {
+            try {
+                throw new Exception("Matrix 1 and 2 have different dimensions!");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.exit(-1);
             }
         }
 
-        for (int i = 0; i < cnt; i++) {
-            threads[i].start();
+        threadsCnt = Math.min(threadsCnt, matrix1.getRowSize());
+        threadsCnt = findDivider(threadsCnt, matrix1.getRowSize());
+        int step = matrix1.getRowSize() / threadsCnt;
+
+        ExecutorService exec = Executors.newFixedThreadPool(threadsCnt);
+        ArrayList<Future> threads = new ArrayList<>();
+
+        int[][] matrixOfSizesI = new int[threadsCnt][threadsCnt];
+        int[][] matrixOfSizesJ = new int[threadsCnt][threadsCnt];
+
+        int stepI = 0;
+        for (int i = 0; i < threadsCnt; i++) {
+            int stepJ = 0;
+            for (int j = 0; j < threadsCnt; j++) {
+                matrixOfSizesI[i][j] = stepI;
+                matrixOfSizesJ[i][j] = stepJ;
+                stepJ += step;
+            }
+            stepI += step;
         }
 
-        for (int i = 0; i < cnt; i++) {
+        for (int l = 0; l < threadsCnt; l++) {
+            for (int i = 0; i < threadsCnt; i++) {
+                for (int j = 0; j < threadsCnt; j++) {
+                    int stepI0 = matrixOfSizesI[i][j];
+                    int stepJ0 = matrixOfSizesJ[i][j];
+
+                    int stepI1 = matrixOfSizesI[i][(i + l) % threadsCnt];
+                    int stepJ1 = matrixOfSizesJ[i][(i + l) % threadsCnt];
+
+                    int stepI2 = matrixOfSizesI[(i + l) % threadsCnt][j];
+                    int stepJ2 = matrixOfSizesJ[(i + l) % threadsCnt][j];
+
+                    FoxThread t = new FoxThread(copyBlock(matrix1, stepI1, stepJ1, step),
+                            copyBlock(matrix2, stepI2, stepJ2, step), stepI0, stepJ0, resultMatrix);
+                    threads.add(exec.submit(t));
+                }
+            }
+        }
+        for (Future mapFuture : threads) {
             try {
-                threads[i].join();
-            } catch (InterruptedException e) {
+                mapFuture.get();
+            } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
         }
-
+        exec.shutdown();
         return resultMatrix;
     }
+
+    private Matrix copyBlock(Matrix matrix, int i, int j, int size) {
+        Matrix block = new Matrix(size, size);
+        for (int k = 0; k < size; k++) {
+            System.arraycopy(matrix.getMatrix()[k + i], j, block.getMatrix()[k], 0, size);
+        }
+        return block;
+    }
 }
+
 
--- lab2/src/main/java/org/madi/lab2/fox/FoxThread.java
+++ lab2/src/main/java/org/madi/lab2/fox/FoxThread.java
@@ -8,65 +8,35 @@ public class FoxThread extends Thread {
     private final Matrix matrix2;
     private final int curRowShift;
     private final int curColShift;
-    private final int blockSize;
     private final Matrix resultMatrix;
 
-    public FoxThread(Matrix Matrix1, Matrix Matrix2, int curRowShift,
-                     int curColShift, int blockSize, Matrix resultMatrix) {
+    public FoxThread(Matrix Matrix1, Matrix Matrix2, int curRowShift, int curColShift, Matrix resultMatrix) {
         this.resultMatrix = resultMatrix;
         this.matrix1 = Matrix1;
         this.matrix2 = Matrix2;
         this.curRowShift = curRowShift;
         this.curColShift = curColShift;
-        this.blockSize = blockSize;
     }
 
     @Override
     public void run() {
-        int m1RowSize = blockSize;
-        int m2ColSize = blockSize;
+    Matrix blockRes = multiplyBlock();
+    for (int i = 0; i < blockRes.getRowSize(); i++) {
+        for (int j = 0; j < blockRes.getColumnSize(); j++) {
+            resultMatrix.getMatrix()[i + curRowShift][j + curColShift] += blockRes.getMatrix()[i][j];
+        }
+    }
+}
 
-        if (curRowShift + blockSize > matrix1.getRowSize())
-            m1RowSize = matrix1.getRowSize() - curRowShift;
-
-        if (curColShift + blockSize > matrix2.getColumnSize())
-            m2ColSize = matrix2.getColumnSize() - curColShift;
-
-        for (int k = 0; k < matrix1.getRowSize(); k += blockSize) {
-            int m1ColSize = blockSize;
-            int m2RowSize = blockSize;
-
-            if (k + blockSize > matrix2.getRowSize()) {
-                m2RowSize = matrix2.getRowSize() - k;
-            }
-
-            if (k + blockSize > matrix1.getColumnSize()) {
-                m1ColSize = matrix1.getColumnSize() - k;
-            }
-
-            Matrix blockFirst = copyBlock(matrix1, curRowShift, curRowShift + m1RowSize,
-                    k, k + m1ColSize);
-            Matrix blockSecond = copyBlock(matrix2, k, k + m2RowSize,
-                    curColShift, curColShift + m2ColSize);
-
-            Matrix resBlock = new Striped().multiply(blockFirst, blockSecond);
-            for (int i = 0; i < resBlock.getRowSize(); i++) {
-                for (int j = 0; j < resBlock.getColumnSize(); j++) {
-                    resultMatrix.set(i + curRowShift, j + curColShift, resBlock.get(i, j)
-                            + resultMatrix.get(i + curRowShift, j + curColShift));
-                }
+private Matrix multiplyBlock() {
+    Matrix blockRes = new Matrix(matrix1.getColumnSize(), matrix2.getRowSize());
+    for (int i = 0; i < matrix1.getRowSize(); i++) {
+        for (int j = 0; j < matrix2.getColumnSize(); j++) {
+            for (int k = 0; k < matrix1.getColumnSize(); k++) {
+                blockRes.getMatrix()[i][j] += matrix1.getMatrix()[i][k] * matrix2.getMatrix()[k][j];
             }
         }
     }
-
-    private Matrix copyBlock(Matrix src, int rowStart, int rowFinish, int colStart, int colFinish) {
-        Matrix copyMatrix = new Matrix(rowFinish - rowStart, colFinish - colStart);
-        for (int i = 0; i < rowFinish - rowStart; i++) {
-            for (int j = 0; j < colFinish - colStart; j++) {
-                copyMatrix.set(i, j, src.get(i + rowStart, j + colStart));
-            }
-        }
-
-        return copyMatrix;
-    }
+    return blockRes;
+}
 }
