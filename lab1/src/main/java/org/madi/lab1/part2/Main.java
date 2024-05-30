b140e70fe10671669bad5fed13ede80e2a9aa330
madinaadzhi <madinaadzhigeldieva2004@gmail.com>
2024-05-30 14:57:38 +0300

    init lab 6

--- lab1/src/main/java/org/madi/lab1/part2/Main.java
+++ lab1/src/main/java/org/madi/lab1/part2/Main.java
@@ -1,12 +1,18 @@
 package org.madi.lab1.part2;
 
 public class Main {
-    public static void main(String[] args) {
-        CharThread charThread1 = new CharThread('|');
-        CharThread charThread2 = new CharThread('-');
-//        OrderedCharThread charThread1 = new OrderedCharThread('|');
-//        OrderedCharThread charThread2 = new OrderedCharThread('-');
+    public static void main(String[] args) throws InterruptedException {
+//        CharThread charThread1 = new CharThread('|');
+//        CharThread charThread2 = new CharThread('-');
+        OrderedCharThread charThread1 = new OrderedCharThread('|');
+        OrderedCharThread charThread2 = new OrderedCharThread('-');
         charThread1.start();
         charThread2.start();
+        charThread1.join(200000);
+        charThread2.join(200000);
+        System.out.println("Main");
+        System.out.println("Main");
+        System.out.println("Main");
+        System.out.println("Main");
     }
 }
--- mkr2/src/main/java/org/madi/mkr2/task19/Main.java
+++ mkr2/src/main/java/org/madi/mkr2/task19/Main.java
@@ -1,31 +1,31 @@
-//package org.madi.mkr2.task19;
-//
-//import mpi.*;
-//
-//public class Main {
-//    public static void main(String[] args) throws MPIException {
-//        MPI.Init(args);
-//        int size = MPI.COMM_WORLD.Size();
-//        int rank = MPI.COMM_WORLD.Rank();
-//
-//        if (rank == 2) {
-//            double[] temperatureData = generateTemperatureData();
-//            MPI.COMM_WORLD.Send(temperatureData, 0, temperatureData.length, MPI.DOUBLE, 0, 0);
-//        }
-//
-//        if (rank == 0) {
-//            double[] receivedData = new double[30];
-//            MPI.COMM_WORLD.Recv(receivedData, 0, receivedData.length, MPI.DOUBLE, 2, 0);
-//            System.out.println("Received temperature data from rank 2:");
-//            for (int i = 0; i < receivedData.length; i++) {
-//                System.out.println("Day " + (i + 1) + ": " + receivedData[i]);
-//            }
-//        }
-//        MPI.Finalize();
-//    }
-//
-//    private static double[] generateTemperatureData() {
-//        double[] temperatureData = new double[30];
-//        return temperatureData;
-//    }
-//}
+package org.madi.mkr2.task19;
+
+import mpi.*;
+
+public class Main {
+    public static void main(String[] args) throws MPIException {
+        MPI.Init(args);
+        int size = MPI.COMM_WORLD.Size();
+        int rank = MPI.COMM_WORLD.Rank();
+
+        if (rank == 2) {
+            double[] temperatureData = generateTemperatureData();
+            MPI.COMM_WORLD.Send(temperatureData, 0, temperatureData.length, MPI.DOUBLE, 0, 0);
+        }
+
+        if (rank == 0) {
+            double[] receivedData = new double[30];
+            MPI.COMM_WORLD.Recv(receivedData, 0, receivedData.length, MPI.DOUBLE, 2, 0);
+            System.out.println("Received temperature data from rank 2:");
+            for (int i = 0; i < receivedData.length; i++) {
+                System.out.println("Day " + (i + 1) + ": " + receivedData[i]);
+            }
+        }
+        MPI.Finalize();
+    }
+
+    private static double[] generateTemperatureData() {
+        double[] temperatureData = new double[30];
+        return temperatureData;
+    }
+}
